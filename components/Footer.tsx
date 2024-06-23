960c999abc9447d57e273beb3a614cd9249f1a47
Gayathri Yedavilli <gyedavil@uci.edu>
2024-06-23 00:40:49 -0700

    fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 
 const Footer = () => {
-  const styles = {
-    footerContainer: {
-      backgroundColor: "#FDEEF7",
-      padding: "1rem 4rem",
-      borderTop: "2px solid #F7A8B8",
-      textAlign: "center",
-      fontFamily: "'Poppins', sans-serif",
-      color: "#333",
-      bottom: 0,
-      left: 0,
-      width: "100%",
-      position: "fixed" as "fixed",
-    },
-  };
-
   return (
-    <footer style={styles.footerContainer}>
-      <p>&copy; made with 💖 in berkeley.</p>
+    <footer
+      style={{
+        backgroundColor: "#FDEEF7",
+        padding: "1rem 4rem",
+        borderTop: "2px solid #F7A8B8",
+        textAlign: "center" as "center",
+        fontFamily: "'Poppins', sans-serif",
+        color: "#333",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        position: "fixed" as "fixed",
+      }}
+    >
+      <p>&copy; made with 💖 in Berkeley.</p>
     </footer>
   );
 };
