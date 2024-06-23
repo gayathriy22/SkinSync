30cb5dea1286621f8adba112cd3e6820a54e1021
Elise Ji <ccelisej.04@gmail.com>
2024-06-23 00:18:55 -0700

    this one right

--- app/firebase.ts
+++ app/firebase.ts
@@ -9,15 +9,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBahZi4kvD3Ml5PmVvE1SyEqkYYL59MRrk",
-    authDomain: "berkeleyhacks24.firebaseapp.com",
-    projectId: "berkeleyhacks24",
-    storageBucket: "berkeleyhacks24.appspot.com",
-    messagingSenderId: "657471536183",
-    appId: "1:657471536183:web:186128126ac79e4b1553b2",
-    measurementId: "G-QBZFW1XJJM"
-  };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
