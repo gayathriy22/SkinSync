1f1b8cd9ed89a3988685180a45f035bc38ce6dd5
Gayathri Yedavilli <gyedavil@uci.edu>
2024-06-22 21:34:45 -0700

    added dummy navbar component that doesnt work

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,69 @@
+// import React from 'react';
+
+// const NavBar = () => {
+//   return (
+//     <NavContainer>
+//       <Logo>skinSync</Logo>
+//       <NavLinks>
+//         <NavLink href="#" className="active">Home</NavLink>
+//         <NavLink href="#">My Skin</NavLink>
+//         <NavLink href="#">Blog</NavLink>
+//       </NavLinks>
+//       <SignInButton href="#">Sign In &#8594;</SignInButton>
+//     </NavContainer>
+//   );
+// };
+
+// export default NavBar;
+
+// const NavContainer = styled.nav`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   padding: 1rem 2rem;
+//   background-color: #FDEEF7;
+//   border-bottom: 2px solid #F7A8B8;
+// `;
+
+// const Logo = styled.div`
+//   font-size: 1.5rem;
+//   font-weight: bold;
+//   color: #D17EB6;
+//   font-family: 'Poppins', sans-serif;
+// `;
+
+// const NavLinks = styled.div`
+//   display: flex;
+//   gap: 2rem;
+// `;
+
+// const NavLink = styled.a`
+//   font-size: 1rem;
+//   text-decoration: none;
+//   color: black;
+//   font-family: 'Poppins', sans-serif;
+
+//   &.active {
+//     color: #D17EB6;
+//     font-weight: bold;
+//   }
+
+//   &:hover {
+//     text-decoration: underline;
+//   }
+// `;
+
+// const SignInButton = styled.a`
+//   padding: 0.5rem 1rem;
+//   background-color: #D17EB6;
+//   color: white;
+//   border-radius: 5px;
+//   text-decoration: none;
+//   font-family: 'Poppins', sans-serif;
+//   display: flex;
+//   align-items: center;
+
+//   &:hover {
+//     background-color: #BF5D8C;
+//   }
+// `;
